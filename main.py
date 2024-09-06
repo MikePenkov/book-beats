41d8f4fbce82bce1c9e326d90c8c402fcfe1e89c
Spas Z <spaszahariev54@gmail.com>
2024-09-06 20:43:06 +0200

    working and coming back with a code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 import threading
 import webbrowser
 from dotenv import load_dotenv
 import os
+from multiprocessing import Process
 
 load_dotenv()
 
@@ -24,33 +25,39 @@ def run_app():
 
 @app.route('/')
 def index():
+
 #    return AUTHORIZE_URL 
-  return f"<a href='{AUTHORIZE_URL}'>Authorize</a>" 
- #  return redirect(AUTHORIZE_URL, code=302)
+  #return f"<a href='{AUTHORIZE_URL}'>Authorize</a>" 
+  return redirect(AUTHORIZE_URL, code=302)
 
 @app.route('/callback')
 def callback():
     global received_code
     received_code = request.args.get('code')
     if received_code:
-        shutdown_server()  # Shut down the server after receiving the code
+      #  shutdown_server()  # Shut down the server after receiving the code
         return f"<h1>Authorization Code:</h1><code>{received_code}</code>"
     else:
         return "<h1>Error: No code found in the callback URL</h1>", 400
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+# def shutdown_server():
+#     func = request.environ.get('werkzeug.server.shutdown')
+#     if func is None:
+#         raise RuntimeError('Not running with the Werkzeug Server')
+#     func()
 
 if __name__ == '__main__':
+    global server
     threading.Thread(target=run_app).start()  # Run Flask app in a separate thread
+    server = Process(target=run_app)
+    server.start()
     webbrowser.open('http://localhost:5000/')
 
     while received_code is None:
         pass  # Wait until the authorization code is received
 
+    server.terminate()
+    server.join()
     print(f"Received authorization code: {received_code}")
     user_input = input("Please enter something to continue: ")
     print(f"You entered: {user_input}")
